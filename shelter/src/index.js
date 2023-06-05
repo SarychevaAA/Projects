98190568153a88465558fbd2cd4fa03d95d48d19
Aleksandra <alex.sarychewa2014@yandex.ru>
2023-06-06 01:11:43 +0300

    fix: remove description console.log

--- shelter/src/index.js
+++ shelter/src/index.js
@@ -2,7 +2,6 @@ import './pages/index.html'
 import './pages/pets.html'
 import './style/index.scss'
 
-import './js/description-task'
 import './js/slider'
 import './js/pagination'
 import './js/modal-window'
